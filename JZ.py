a3e6f43224918c3101ee49a0afac1c9d36a22a05
Jaishak J <99122415+jaishakj@users.noreply.github.com>
2023-04-18 22:02:26 +0530

    Update JZ.py
    The programming is converting my  voice into text and change it text then convert the text into which language you want and convert the text into anime voice using VoiceVox software .

--- JZ.py
+++ JZ.py
@@ -1,1 +1,30 @@
+import speech_recognition as sr
+from googletrans import Translator
+import subprocess
+
+# Initialize recognizer
+r = sr.Recognizer()
+
+# Use microphone as audio source
+with sr.Microphone() as source:
+    print("Speak something...")
+    audio = r.listen(source)
+
+# Use Google Speech Recognition to convert audio to text
+try:
+    text = r.recognize_google(audio)
+    print("You said: {}".format(text))
+
+    # Translate the text
+    translator = Translator()
+    translated_text = translator.translate(text, dest='es').text
+    print("Translated text: {}".format(translated_text))
+
+    # Use Voicevox to convert text to speech
+    command = ['open', '-a', 'Voicevox', '--args', translated_text]
+    subprocess.Popen(command)
+except sr.UnknownValueError:
+    print("Unable to recognize speech")
+except sr.RequestError as e:
+    print("Error: {}".format(e))
 
