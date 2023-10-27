f9b40a1fd8b03503be2acbdb12c4e96384e1c934
Jaishak J <99122415+jaishakj@users.noreply.github.com>
2023-10-27 21:13:28 +0530

    Create Return Values in store and retrieve in Cloudstorage.py
    The store method should return True if the data is successfully stored, and the retrieve method should return the actual data retrieved from the cloud, rather than just a placeholder string.

---
+++ .devcontainer/Return Values in store and retrieve in Cloudstorage.py
@@ -0,0 +1,10 @@
+class CloudStorage:
+    def store(self, data):
+        # Implement logic to store data in the cloud
+        # Example: Connect to cloud service, upload data
+        return True  # Placeholder logic
+
+    def retrieve(self):
+        # Implement logic to retrieve data from the cloud
+        # Example: Connect to cloud service, download data
+        return "Brain data"  # Placeholder data
